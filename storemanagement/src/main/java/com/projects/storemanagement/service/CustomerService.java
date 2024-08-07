64678759c6136ec362eeac0bf02ac50829b7c6bf
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-07 23:48:20 +0300

    Added @Service annotation, methods refactoring: methods name changes to respect naming conventions, setId to null in create and to id in update

--- storemanagement/src/main/java/com/projects/storemanagement/service/CustomerService.java
+++ storemanagement/src/main/java/com/projects/storemanagement/service/CustomerService.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public interface CustomerService {
 
-    List<Customer> findAllCustomers();
-    Customer findCustomer(Long id);
-    Customer saveCustomer(Customer customer);
-    Customer updateCustomer(Customer customer);
+    Customer findById(Long id);
+    List<Customer> findAll();
+    Customer create(Customer customer);
+    Customer update(Long id, Customer customer);
 
 }
--- storemanagement/src/main/java/com/projects/storemanagement/service/CustomerServiceImpl.java
+++ storemanagement/src/main/java/com/projects/storemanagement/service/CustomerServiceImpl.java
@@ -4,35 +4,39 @@ import com.projects.storemanagement.entity.Customer;
 import com.projects.storemanagement.exception.CustomerNotFoundException;
 import com.projects.storemanagement.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
 
     @Override
-    public List<Customer> findAllCustomers() {
-        return customerRepository.findAll();
-    }
-
-    @Override
-    public Customer findCustomer(Long id) {
+    public Customer findById(Long id) {
         return customerRepository.findById(id)
                 .orElseThrow(() -> new CustomerNotFoundException("Customer not found with id " + id));
     }
 
     @Override
-    public Customer saveCustomer(Customer customer) {
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        customer.setId(null);
         return customerRepository.save(customer);
     }
 
     @Override
-    public Customer updateCustomer(Customer customer) {
-        if (!customerRepository.existsById(customer.getId())) {
-            throw new CustomerNotFoundException("Customer not found with id " + customer.getId());
+    public Customer update(Long id, Customer customer) {
+        if (!customerRepository.existsById(id)) {
+            throw new CustomerNotFoundException("Customer not found with id " + id);
         }
+        customer.setId(id);
         return customerRepository.save(customer);
     }
 
