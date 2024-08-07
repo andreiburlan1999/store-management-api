e6a3137c2c8207e0f13a7fbbbfca8bbe7529dfb9
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-07 21:31:42 +0300

    Update sequence name for Product entity

--- storemanagement/src/main/java/com/projects/storemanagement/entity/Product.java
+++ storemanagement/src/main/java/com/projects/storemanagement/entity/Product.java
@@ -13,8 +13,8 @@ import lombok.Setter;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
-    @SequenceGenerator(name = "product_seq", sequenceName = "product_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product")
+    @SequenceGenerator(name = "seq_product", sequenceName = "seq_product", allocationSize = 1)
     private Long id;
 
     private String name;
