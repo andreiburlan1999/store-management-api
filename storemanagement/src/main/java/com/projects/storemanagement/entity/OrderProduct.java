7485b0dc259ecefea6bddb911830722f7019d151
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-07 21:43:46 +0300

    Add OrderProduct entity

---
+++ storemanagement/src/main/java/com/projects/storemanagement/entity/OrderProduct.java
@@ -0,0 +1,32 @@
+package com.projects.storemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "order_product")
+public class OrderProduct {
+
+    @EmbeddedId
+    private OrderProductPk id;
+
+    @ManyToOne
+    @MapsId("orderId")
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @MapsId("productId")
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    private Double price;
+
+}
