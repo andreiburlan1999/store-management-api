1492ea41297e2f1801a84e7f4a63f877428f1511
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-07 21:13:03 +0300

    Add User entity

---
+++ storemanagement/src/main/java/com/projects/storemanagement/entity/User.java
@@ -0,0 +1,30 @@
+package com.projects.storemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+    @SequenceGenerator(name = "seq_user", sequenceName = "seq_user", allocationSize = 1)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+}
