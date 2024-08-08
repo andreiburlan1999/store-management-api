ad5cb79eb0168986adaa84a0f419333c6e0c0ba2
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-08 20:04:56 +0300

    Change responses of UserController to ResponseEntity

--- storemanagement/src/main/java/com/projects/storemanagement/controller/UserController.java
+++ storemanagement/src/main/java/com/projects/storemanagement/controller/UserController.java
@@ -3,6 +3,8 @@ package com.projects.storemanagement.controller;
 import com.projects.storemanagement.entity.User;
 import com.projects.storemanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,31 +17,31 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
         User user = userService.findById(id);
         user.setPassword("*");
-        return user;
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping
-    public List<User> getAllUsers() {
+    public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.findAll();
         users.forEach(user -> user.setPassword("*"));
-        return users;
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.create(user);
         createdUser.setPassword("*");
-        return createdUser;
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     @PutMapping("/{id}")
-    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         User updatedUser = userService.update(id, user);
         updatedUser.setPassword("*");
-        return updatedUser;
+        return ResponseEntity.ok(updatedUser);
     }
 
     @GetMapping("/current")
