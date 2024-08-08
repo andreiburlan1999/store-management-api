8059f72a50db0f51a5d7e6e53e513b545418f9ee
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-08 19:54:01 +0300

    Mask users password on response

--- storemanagement/src/main/java/com/projects/storemanagement/controller/UserController.java
+++ storemanagement/src/main/java/com/projects/storemanagement/controller/UserController.java
@@ -16,27 +16,37 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
-        return userService.findById(id);
+        User user = userService.findById(id);
+        user.setPassword("*");
+        return user;
     }
 
     @GetMapping
     public List<User> getAllUsers() {
-        return userService.findAll();
+        List<User> users = userService.findAll();
+        users.forEach(user -> user.setPassword("*"));
+        return users;
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
-        return userService.create(user);
+        User createdUser = userService.create(user);
+        createdUser.setPassword("*");
+        return createdUser;
     }
 
     @PutMapping("/{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
-        return userService.update(id, user);
+        User updatedUser = userService.update(id, user);
+        updatedUser.setPassword("*");
+        return updatedUser;
     }
 
     @GetMapping("/current")
     public User getCurrentUser() {
-        return userService.getCurrentUser();
+        User user = userService.getCurrentUser();
+        user.setPassword("*");
+        return user;
     }
 
 }
