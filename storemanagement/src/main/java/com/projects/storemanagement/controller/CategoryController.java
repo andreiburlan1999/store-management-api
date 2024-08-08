7d2dec177cdcb4378536ba8b74f342f95dffdaa0
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-08 20:23:59 +0300

    Add CategoryController

---
+++ storemanagement/src/main/java/com/projects/storemanagement/controller/CategoryController.java
@@ -0,0 +1,42 @@
+package com.projects.storemanagement.controller;
+
+import com.projects.storemanagement.entity.Category;
+import com.projects.storemanagement.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        Category category = categoryService.findById(id);
+        return ResponseEntity.ok(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category createdCategory = categoryService.create(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Category updatedCategory = categoryService.update(id, category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+}
