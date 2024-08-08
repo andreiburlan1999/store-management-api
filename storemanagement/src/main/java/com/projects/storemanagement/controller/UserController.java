ab19f3704bbaeb3ed45131c5f73837874eaf458a
Andrei Burlan <andrei.burlan@ibm.com>
2024-08-08 19:32:23 +0300

    Add UserController

---
+++ storemanagement/src/main/java/com/projects/storemanagement/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.projects.storemanagement.controller;
+
+import com.projects.storemanagement.entity.User;
+import com.projects.storemanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @GetMapping("/current")
+    public User getCurrentUser() {
+        return userService.getCurrentUser();
+    }
+
+}
